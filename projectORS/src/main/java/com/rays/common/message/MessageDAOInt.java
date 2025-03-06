f0bc37287660b581d028cf60f7a9e1af2477e333
SanjayVerma52 <rohitsarvan244@gmail.com>
2025-03-06 14:57:00 +0530

    done

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Rohit Rajput 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
---
+++ projectORS/src/main/java/com/rays/dao/CollegeDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.CollegeDTO;
+
+/**
+ * College DAO interface.
+ * Rohit Rajput 
+ */
+public interface CollegeDAOInt extends BaseDAOInt<CollegeDTO> {
+}
